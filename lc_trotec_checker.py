d8226782fb918d68c3eff69fb90f411d8f44bd70
Julien Allali <allali.julien@gmail.com>
2018-11-26 11:11:15 +0100

    add support for image

--- lc_trotec_checker.py
+++ lc_trotec_checker.py
@@ -6,10 +6,16 @@ sys.path.append('/usr/share/inkscape/extensions')
 import inkex
 from simplestyle import *
 
+import PIL
+from PIL import Image
+import base64
+
 import simpletransform
 
 import lc
 import re
+
+import tempfile
 
 # List of color codes used by Trotec
 acceptable_colors=['#000000','#ff0000','#0000ff','#336699','#00ffff','#00ff00','#009933','#006633','#9900cc','#ff00ff','#ff6600','#ffff00','#999933','#996633','#663300','#660066']
@@ -35,6 +41,38 @@ class TrotecChecker(inkex.Effect):
                                      help = 'remove filter')
         
     def apply(self,node):
+        if node.tag == inkex.addNS('image', 'svg') or node.tag == 'image':
+            inkex.debug("image found! ")
+            data=node.get(inkex.addNS('href','xlink'))            
+            # must check image is embeded
+            if data[:5]!='data:':
+                inkex.debug("image is not embeded: we skip it!")
+            else:
+                i=data.find("image/")
+                j=data.find(";")
+                if i==-1 or j==-1:
+                    inkex.debug("image data: failed to find image format!")
+                else:
+                    img_format=data[i+6:j]
+                    img_data_header_pos=data.find(',')
+                    img_data=base64.decodestring(data[img_data_header_pos+1:])
+                    f=tempfile.NamedTemporaryFile(mode='w+b',suffix="."+img_format,delete=True)
+                    f.write(img_data)
+                    f.flush()
+                    img=PIL.Image.open(f.name)
+                    if img.mode in ('RGBA', 'LA') or (img.mode == 'P' and 'transparency' in img.info):
+                        inkex.debug("image has an alpha channel... remove it!")
+                        alpha = img.convert('RGBA').split()[-1] # get the alpha channel
+                        bg = Image.new("RGB", img.size, (255,255,255)) # create the background
+                        bg.paste(img, mask=alpha) # merge background and image
+                        bg.show()
+                        bg.save(f.name)
+                        f.seek(0)
+                        ndata=f.read()
+                        ndata=base64.encodestring(ndata)
+                        ndata=data[:img_data_header_pos]+','+ndata
+                        node.set(inkex.addNS('href','xlink'),ndata)
+                    f.close()
         if 'style' in node.attrib:
             style=node.get('style') 
             if style!='':
@@ -83,7 +121,7 @@ class TrotecChecker(inkex.Effect):
                             inkex.debug("TODO: filter found, should be removed")
                     elif s.startswith("stroke-width:"):
                         if self.options.fixstroke:
-                            styles[i]=str(self.strokewidth)
+                            styles[i]='stroke-width:'+str(self.strokewidth)
                 if hasFill and hasStroke:
                     inkex.debug("WARNING: suspicious element that has both fill and stroke!")
                 node.set('style',";".join(styles))
@@ -100,7 +138,7 @@ class TrotecChecker(inkex.Effect):
                         inkex.debug("INFO: a group have a clipping path: removed")
                     else:
                         inkex.debug("TODO: a group have a clipping path: you must remove it!")
-                return self.recursivelyTraverse(node)
+                self.recursivelyTraverse(node)
             else:
                 self.apply(node)
         return result
